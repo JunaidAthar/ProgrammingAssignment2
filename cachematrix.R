96ed8407ca71cd313c8ea9e2d73646621731f9e4
junaidathar <junaid.athar@gmail.com>
2015-06-13 12:52:20 -0500

    Changes for Class

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes the inverse of a Matrix
 
-## Write a short comment describing this function
+
+## An intervtible Matrix's inversion.
 
 makeCacheMatrix <- function(x = matrix()) {
+
+x^-1
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	x^^-1
 }
